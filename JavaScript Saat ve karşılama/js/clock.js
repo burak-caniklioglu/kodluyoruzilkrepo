2071cbdfe323a18f614970cafcdf2e50776c7f4e
burak-caniklioglu <burakcaniklioglu@gmail.com>
2022-03-13 17:47:18 +0300

    Bug fixed

--- JavaScript Saat ve karşılama/js/clock.js
+++ JavaScript Saat ve karşılama/js/clock.js
@@ -2,11 +2,22 @@ let name = prompt("Adınızı Giriniz")
 let myName = document.querySelector("#myName")
 myName.innerHTML = `${name}`
 let turkce = ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi","Pazar"];
-const today = new Date()
 
-let day = today.getDay()
-let h = today.getHours()
-let m = today.getMinutes()
-let s = today.getSeconds()
-let clock_and_day = document.querySelector("#myClock").innerHTML = `${h}:${m}:${s} ${turkce[day-1]}`
-setTimeout(startTime, 1000)
+
+
+let clock_and_day = document.querySelector("#myClock")
+
+
+
+
+function clock() {
+    const today = new Date()
+    let day = today.getDay()
+    let h = today.getHours().toString().padStart(2,"0")
+    let m = today.getMinutes().toString().padStart(2,"0")
+    let s = today.getSeconds().toString().padStart(2,"0")
+    clock_and_day.innerText = `${h}:${m}:${s} ${turkce.at(day-1)}`
+}
+
+clock()
+setInterval(clock, 1000)
